24f35246d09b3299a694cead872370cf3d8c4dfe
DACL <DACL@NB-DACL.local.parcit>
2019-06-05 19:06:33 +0200

    Added two Main Classes - One for JavaFX and one for Spring Web

--- src/zur/koeln/kickertool/SpringWebMain.java
+++ src/zur/koeln/kickertool/SpringWebMain.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class SpringWebMain {
 
     public static void main(String[] args) {
     	
-    	SpringApplication.run(Main.class);
+    	SpringApplication.run(SpringWebMain.class);
         
     }
 
