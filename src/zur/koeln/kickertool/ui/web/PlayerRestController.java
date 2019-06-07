7eb0adaec955ea88b26ee1da18c26d685cc02536
Daniel Cleemann <Daniel.Cleemann@gmx.de>
2019-06-07 22:51:25 +0200

    Rest API for Players

--- src/zur/koeln/kickertool/ui/web/PlayerRestController.java
+++ src/zur/koeln/kickertool/ui/web/PlayerRestController.java
@@ -1,26 +1,39 @@
 package zur.koeln.kickertool.ui.web;
 
-import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import lombok.AccessLevel;
+import lombok.Getter;
 import zur.koeln.kickertool.application.handler.api.IPlayerCommandHandler;
 import zur.koeln.kickertool.application.handler.dtos.PlayerDTO;
 import zur.koeln.kickertool.application.handler.dtos.base.ListResponseDTO;
+import zur.koeln.kickertool.application.handler.dtos.base.SingleResponseDTO;
+import zur.koeln.kickertool.application.handler.dtos.base.StatusOnlyDTO;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
+@RequestMapping("/api/players")
 public class PlayerRestController {
 
 	@Autowired
+	@Getter(value = AccessLevel.PRIVATE)
 	private IPlayerCommandHandler commandHandler;
-	
-	@RequestMapping("/getAll")
-	public List<PlayerDTO> getAllPlayer() {
-		
-		ListResponseDTO<PlayerDTO> allPlayer = commandHandler.getAllPlayer();
-		return allPlayer.getDtoValueList();
+
+	@GetMapping("/getall")
+	public ListResponseDTO<PlayerDTO> getAllPlayer() {
+		return getCommandHandler().getAllPlayer();
 	}
-	
+
+	@PostMapping()
+	public SingleResponseDTO<PlayerDTO> insertNewPlayer(@RequestBody PlayerDTO playerDTO) {
+		return getCommandHandler().createNewPlayer(playerDTO.getFirstName(), playerDTO.getLastName());
+	}
+
+	@DeleteMapping("/{uuid}")
+	public @ResponseBody StatusOnlyDTO deletePlayer(@PathVariable UUID uuid) {
+		return getCommandHandler().deletePlayer(uuid);
+	}
 }
